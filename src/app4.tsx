e7bcb11d4601d091fb3e5dbedaad1c11314d0c1f
a-achouri <achouri.abdelilah@hotmail.com>
2022-12-29 11:22:22 +0100

    aa

---
+++ src/app4.tsx
@@ -0,0 +1,15 @@
+// import React, { useState } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import AppsTable from './components/Apps/AppsTable';
+// import Achouri from './components/Achouri';
+import AppsIndex from './components/AppsIndex';
+
+function App4() {
+
+  return (
+  
+  );
+}
+
+export default App4;
